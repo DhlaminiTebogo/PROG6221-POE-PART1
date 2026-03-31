7c0a92bf6e5b086d9f37c30c27892603cab10dd3
DhlaminiTebogo <ST10487180@imconnect.edu.za>
2026-03-31 15:35:35 +0200

    Update ConsoleUIMethods.cs
    Added functionality to the ConsoleUIMethods class. there is ascii art

--- ConsoleUIMethods.cs
+++ ConsoleUIMethods.cs
@@ -4,5 +4,50 @@ public class ConsoleUIMethods
 {
 	public ConsoleUIMethods()
 	{
-	}
+
+        public static void PrintBanner()
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("============================================");
+        Console.WriteLine(@"
+    ___      _               _           _   
+   / __\   _| |__   ___ _ __| |__   ___ | |_ 
+  / /| | | | '_ \ / _ \ '__| '_ \ / _ \| __|
+ / /_| |_| | |_) |  __/ |  | |_) | (_) | |_ 
+/____/\__, |_.__/ \___|_|  |_.__/ \___/ \__|
+      |___/                                  
+        ");
+        Console.WriteLine("============================================");
+        Console.ResetColor();
+    }
+
+    public static void PrintBot(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.Write("Bot: ");
+        Console.ResetColor();
+        Console.WriteLine(message);
+    }
+
+    public static void PrintUser()
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write("You: ");
+        Console.ResetColor();
+    }
+
+    public static void PrintError(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ResetColor();
+    }
+
+    public static void PrintDivider()
+    {
+        Console.ForegroundColor = ConsoleColor.DarkCyan;
+        Console.WriteLine("--------------------------------------------");
+        Console.ResetColor();
+    }
+}
 }
