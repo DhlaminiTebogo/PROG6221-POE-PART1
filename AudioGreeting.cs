edc138096788613c1ed6706220978395642e9b8c
DhlaminiTebogo <ST10487180@imconnect.edu.za>
2026-03-31 13:47:24 +0200

    Add project files.

---
+++ AudioGreeting.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class AudioGreeting
+{
+	public AudioGreeting()
+	{
+	}
+}
---
+++ ConsoleUIMethods.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class ConsoleUIMethods
+{
+	public ConsoleUIMethods()
+	{
+	}
+}
